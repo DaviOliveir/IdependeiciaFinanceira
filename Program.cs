8c91ba9df7f0f60878e0dd8994d7921b2a3ce89c
Davi Jose Batista Lima de Oliveira <davibatistalimadeoliveira@gmail.com>
2022-09-17 09:41:57 -0300

    idependencia financeira concluido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿decimal GastosMensais, ValorAinvestir;
+double dividendYield;
+
+Console.WriteLine("---vivendo de dividendos ---\n");
+
+
+Console.Write("Gastos Mensais (em R$)...:");
+
+GastosMensais = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("rendimentos mensais(%)...:");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+
+ValorAinvestir = GastosMensais / Convert.ToDecimal(dividendYield / 100);
+Console.WriteLine($"\nPara receber {GastosMensais:C} Por Mês Com Redimentos de {dividendYield:N1}% você precisa ter investido {ValorAinvestir:C}. ");
+
+
